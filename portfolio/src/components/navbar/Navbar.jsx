556cbe3c8226a16d05ee37547529c75788bbf057
VitorTeclak <vitorteclak@hotmail.com>
2026-08-10 15:23:34 -0300

    [NEW] Criado Navbar

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import './Navbar.css'
+
+//dependencias
+import { Link } from 'react-router-dom'
+
+
+//pages
+import Home from '../../pages/home/Home'
+import Sobre from '../../pages/sobre/Sobre'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div>
+        <span>vitor.dev</span>
+      </div>
+      <div className='links-navbar'>
+        <Link to="/" >Home</Link>
+        <Link to="/sobre" >Sobre</Link>
+        <Link to="/projetos">Portfólio</Link>
+      </div>
+    </div>
   )
 }
 
