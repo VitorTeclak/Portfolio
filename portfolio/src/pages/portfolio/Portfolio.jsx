160862edd3a0b479731cf32d9f5aace0ecafe1bf
VitorTeclak <vitorteclak@hotmail.com>
2026-08-11 13:52:00 -0300

    [UPDATE] Alterado conteudo do portfolio

--- portfolio/src/pages/portfolio/Portfolio.jsx
+++ portfolio/src/pages/portfolio/Portfolio.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import './Portfolio.css'
+
+import Projetos from '../../components/projetos/Projetos';
+import Tecnologias from '../../components/tecnologias/Tecnologias'
+
+
+
+import { useState } from 'react';
 
 const Portfolio = () => {
+
+  const [botaoSelecionado, setBotaoSelecionado] = useState('projetos');
+
   return (
-    <div>Portfolio</div>
+    <div>
+      <div className='formatacao-portfolio'>
+        <h1>
+          Vitrine de Portfólio
+        </h1>
+
+        <span className='texto-cinza'>
+          Explore meus projetos, meus certificados e minhas experiencias tecnicas
+        </span>
+
+        <div className='navbar-portfolio'>
+          <button
+            className={botaoSelecionado === 'projetos' ? 'selecionado' : ''}
+            onClick={() => setBotaoSelecionado('projetos')}
+          >
+            Projetos
+          </button>
+
+          <button
+            className={botaoSelecionado === 'stacks' ? 'selecionado' : ''}
+            onClick={() => setBotaoSelecionado('stacks')}
+          >
+            Stacks
+          </button>
+        </div>
+
+        {botaoSelecionado === 'projetos' && <Projetos />}
+
+        {botaoSelecionado === 'stacks' && <Tecnologias />}
+
+
+        
+        
+        
+
+        
+      </div>
+    </div>
   )
 }
 
